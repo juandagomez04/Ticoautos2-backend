8e282466a17fff6728def5e9e8e5c13f9937dde9
juandagomez04 <jdgomezcubillo2004@gmail.com>
2026-05-02 23:58:10 -0600

    fix: actualizar rutas de autenticacion (verificaciones de usuario)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const router = express.Router();
+const router  = express.Router();
 
-const { register, token, googleAuth, validateCedula } = require("../controllers/auth.controller");
+const { register, token, verify2FA, googleAuth, validateCedula, verifyEmail } = require("../controllers/auth.controller");
 const { authenticateToken } = require("../middleware/auth.middleware");
 
-router.post("/register", register);
-router.post("/token", token);
-router.post("/google", googleAuth);
+router.post("/register",      register);
+router.post("/token",         token);
+router.post("/verify-2fa",    verify2FA);
+router.post("/google",        googleAuth);
 router.get("/cedula/:numero", validateCedula);
+router.get("/verify/:token",  verifyEmail);
 
-// endpoint de prueba (protegido)
+// Endpoint protegido de prueba
 router.get("/me", authenticateToken, (req, res) => {
     res.json({ message: "OK ✅", user: req.user });
 });
